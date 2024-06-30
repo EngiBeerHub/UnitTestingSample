efa4cb37064213da00f111a4c324ab7e63321de5
EngiBeerHub <engibeer0422@gmail.com>
2024-06-30 14:59:46 +0900

    Enclosed Test

--- app/src/test/java/com/example/unittestingsample/RepositoryLocalDataSourceTest.kt
+++ app/src/test/java/com/example/unittestingsample/RepositoryLocalDataSourceTest.kt
@@ -4,39 +4,65 @@ import androidx.room.Room
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Before
 import org.junit.Test
-import org.junit.jupiter.api.Assertions.*
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.assertj.core.api.Assertions.*
+import org.junit.After
+import org.junit.experimental.runners.Enclosed
 
-@RunWith(RobolectricTestRunner::class)
-class RepositoryLocalDataSourceTest {
-    lateinit var repositoryLocalDataSource: RepositoryLocalDataSource
+@RunWith(Enclosed::class)
+class EnclosedTest {
+    abstract class DBTest {
+        lateinit var repositoryLocalDataSource: RepositoryLocalDataSource
 
-    @Before
-    fun setUp() {
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        val db = Room
-            .databaseBuilder(context, AppDatabase::class.java, "DB")
-            .allowMainThreadQueries()
-            .build()
-        repositoryLocalDataSource = RepositoryLocalDataSource(db)
+        @Before
+        fun setUpParent() {
+            val context = InstrumentationRegistry.getInstrumentation().targetContext
+            val db = Room
+                .databaseBuilder(context, AppDatabase::class.java, "DB")
+                .allowMainThreadQueries()
+                .build()
+            repositoryLocalDataSource = RepositoryLocalDataSource(db)
+        }
+
+        @After
+        fun tearDownParent() {
+        }
     }
 
-    @Test
-    fun findByOwner_expectsEmpty() {
-        val list = repositoryLocalDataSource.findByOwner("shiroyama")
-        assertThat(list).isEmpty()
+    @RunWith(RobolectricTestRunner::class)
+    class BlankRecord : DBTest() {
+        @Test
+        fun insertAll_successfully_persist_record() {
+            repositoryLocalDataSource.insertAll(
+                Repository(0, "hello", "hello", "shiroyama")
+            )
+            val shiroyamaOwners = repositoryLocalDataSource.findByOwner("shiroyama")
+            assertThat(shiroyamaOwners).hasSize(1)
+        }
     }
 
-    @Test
-    fun insertAll_finishesSuccessfully() {
-        val owner = "shiroyama"
-        repositoryLocalDataSource.insertAll(
-            Repository(1, "hello", "hello", owner),
-            Repository(2, "world", "world", owner)
-        )
-        val list = repositoryLocalDataSource.findByOwner("shiroyama")
-        assertThat(list).hasSize(2)
+    @RunWith(RobolectricTestRunner::class)
+    class RecordPrepared : DBTest() {
+        @Before
+        fun setUp() {
+            repositoryLocalDataSource.insertAll(
+                Repository(1, "hello", "hello", "shiroyama"),
+                Repository(2, "world", "world", "shiroyama"),
+                Repository(3, "yay!", "yay!", "yamazaki"),
+            )
+        }
+
+        @Test
+        fun findByOwner_givenShiroyama_returnsSizeCount2() {
+            val shiroyamaOwners = repositoryLocalDataSource.findByOwner("shiroyama")
+            assertThat(shiroyamaOwners).hasSize(2)
+        }
+
+        @Test
+        fun findByOwner_givenYamazaki_returnsSizeCount1() {
+            val yamazakiOwners = repositoryLocalDataSource.findByOwner("yamazaki")
+            assertThat(yamazakiOwners).hasSize(1)
+        }
     }
 }
